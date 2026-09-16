3474aea6fa1ae5ebda478476b02dab8ab95498b6
Jonatas Sales <sykes.echo@proton.me>
2026-09-16 09:11:15 -0300

    feat(ui): export Product Vision curve

---
+++ packages/react/src/ui/product-vision-curve/index.ts
@@ -0,0 +1,8 @@
+export {
+  ProductVisionCurve,
+  type DriftClassification,
+  type ProductVisionCurveProps,
+  type VisionActor,
+  type VisionDriftEvent,
+  type VisionPoint
+} from './product-vision-curve'
